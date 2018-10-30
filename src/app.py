84f63e255b41dfa8d0334176933a9c2802543f78
arturgspb <art@realweb.ru>
2018-10-30 14:16:55 +0300

    grpc health

--- src/app.py
+++ src/app.py
@@ -1,7 +1,8 @@
 from concurrent import futures
 import time
-
 import grpc
+from grpc_health.v1.health import HealthServicer
+from grpc_health.v1 import health_pb2, health_pb2_grpc
 
 from src.routes_v1 import reg_server_v1
 from src.routes_v2 import reg_server_v2
@@ -12,11 +13,18 @@ _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 def serve(port: int, grace_period: int):
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     server.add_insecure_port('[::]:{}'.format(port))
-    server.start()
 
     # Регистрируем api сервисы
     reg_server_v1(server)
     reg_server_v2(server)
+
+    health = HealthServicer()
+    health.set("plugin", health_pb2.HealthCheckResponse.ServingStatus.Value('SERVING'))
+    health_pb2_grpc.add_HealthServicer_to_server(health, server)
+
+    server.start()
+
+    print("Server started...")
 
     try:
         while True:
